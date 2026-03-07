d5ea16aad37318d10bd4e2321b7ed1c3537e0c04
3RJON1 <erjonjashari08@gmail.com>
2026-03-07 16:39:19 +0100

    3

---
+++ leason 3/chal.py
@@ -0,0 +1,21 @@
+s_gpa = 4.5
+s_score = 75
+
+if s_gpa >= 3.5:
+
+    if 50 <= s_score <= 65:
+        print(f"Student with GPA {s_gpa} and test score of {s_score} may be elgibite for a particle "
+        f"scholarship,")
+
+
+    elif s_score >65:
+        print(f"Student with GPA {s_gpa} and test score of {s_score} may be elgibite for a particle "
+        f"scholarship,")
+
+    else:
+        print(f"Student with GPA {s_gpa} and test score of {s_score} may be elgibite for a particle "
+        f"scholarship,")
+
+else:
+    print(f"Student with GPA {s_gpa} and test score of {s_score} may be elgibite for a particle "
+    f"scholarship,")
